516734f9a67c676912bdea87432d2816c2a4c7b7
kasinico <kasiryenicholas@gmail.com>
2022-08-31 18:56:06 +0300

    TODO PREVIEW AND BACK TO PAGE BUTTON
    Signed-off-by: kasinico <kasiryenicholas@gmail.com>

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -9,15 +9,13 @@ urlpatterns = [
         views.ItemListView.as_view(), name = 'list'),
        # CRUD patterns for ToDoLists
     path("list/add/", views.ListCreate.as_view(), name="list-add"),
-    path(
-        "list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete"
+    path("list/<int:pk>/delete/", views.ListDelete.as_view(), name="list-delete"
     ),
     # CRUD patterns for ToDoItems
     path(
         "list/<int:list_id>/item/add/",
         views.ItemCreate.as_view(),
-        name="item-add",
-    ),
+        name="item-add",    ),
     path(
         "list/<int:list_id>/item/<int:pk>/",
         views.ItemUpdate.as_view(),
